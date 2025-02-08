1c7e4f9eb551ebc7f8fcf9755ed5cde0edaa42ec
hieupnh12 <hieupnh12@gmail.com>
2025-02-08 16:18:59 +0700

    lan dau code

---
+++ src/com/fu/mathutil/MathUtil.java
@@ -0,0 +1,24 @@
+/*
+ */
+package com.fu.mathutil;
+
+public class MathUtil {
+
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid Argument. n must be betten 1 and 20");
+        }
+        //hàm dừng liền k cần return
+
+        // 
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        long product = 1;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+        }
+        return product;
+    }
+
+}
---
+++ src/com/fu/mathutil/MathUtinlLive.java
@@ -0,0 +1,13 @@
+/*
+ */
+
+package com.fu.mathutil;
+
+
+public class MathUtinlLive {
+    public static void main(String[] args) {
+        long result = MathUtil.getFactorial(5);
+        System.out.println("5! = " + result);
+        
+    }
+}
